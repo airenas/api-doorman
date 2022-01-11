86325f4ea8a09ac01f3553e766721d2da943597f
Airenas Vaičiūnas <airenass@gmail.com>
2022-01-11 13:08:09 +0200

    Add Usage tests

--- internal/pkg/integration/cms/handlers_test.go
+++ internal/pkg/integration/cms/handlers_test.go
@@ -98,7 +98,7 @@ func TestAddKey_Fail(t *testing.T) {
 	}
 }
 
-func TestGetKey_Fail(t *testing.T) {
+func TestGetKey(t *testing.T) {
 	type ret struct {
 		key api.Key
 		err error
@@ -125,6 +125,68 @@ func TestGetKey_Fail(t *testing.T) {
 	}
 }
 
+func TestKeyUsage(t *testing.T) {
+	type ret struct {
+		res api.Usage
+		err error
+	}
+	tests := []struct {
+		name   string
+		ret    ret
+		params map[string]string
+		want   int
+	}{
+		{name: "OK", ret: ret{res: api.Usage{RequestCount: 1}, err: nil}, want: http.StatusOK},
+		{name: "Fail", ret: ret{res: api.Usage{RequestCount: 1}, err: errors.New("olia")},
+			want: http.StatusInternalServerError},
+		{name: "Fail", ret: ret{res: api.Usage{RequestCount: 1}, err: api.ErrNoRecord},
+			want: http.StatusBadRequest},
+		{name: "From", ret: ret{res: api.Usage{RequestCount: 1}, err: nil},
+			params: map[string]string{"from": "2020-01-20T14:50:30Z"},
+			want:   http.StatusOK},
+		{name: "To", ret: ret{res: api.Usage{RequestCount: 1}, err: nil},
+			params: map[string]string{"to": "2020-01-20T14:50:30Z"},
+			want:   http.StatusOK},
+		{name: "From fail", ret: ret{res: api.Usage{RequestCount: 1}, err: nil},
+			params: map[string]string{"from": "xx2020-01-20T14:50:30Z"},
+			want:   http.StatusBadRequest},
+		{name: "To fail", ret: ret{res: api.Usage{RequestCount: 1}, err: nil},
+			params: map[string]string{"to": "xx2020-01-20T14:50:30Z"},
+			want:   http.StatusBadRequest},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			initTest(t)
+			pegomock.When(intMock.Usage(pegomock.AnyString(), matchers.AnyPtrToTimeTime(),
+				matchers.AnyPtrToTimeTime(), pegomock.AnyBool())).
+				ThenReturn(&tt.ret.res, tt.ret.err)
+			req := httptest.NewRequest(http.MethodGet, "/key/id1/usage", nil)
+			for k, v := range tt.params {
+				q := req.URL.Query()
+				q.Add(k, v)
+				req.URL.RawQuery = q.Encode()
+			}
+			testCode(t, req, tt.want)
+		})
+	}
+}
+
+func TestKeyUsage_Full(t *testing.T) {
+	initTest(t)
+	pegomock.When(intMock.Usage(pegomock.AnyString(), matchers.AnyPtrToTimeTime(),
+		matchers.AnyPtrToTimeTime(), pegomock.AnyBool())).
+		ThenReturn(&api.Usage{RequestCount: 1}, nil)
+	req := httptest.NewRequest(http.MethodGet, "/key/id1/usage", nil)
+	testCode(t, req, http.StatusOK)
+	intMock.VerifyWasCalledOnce().Usage(pegomock.AnyString(), matchers.AnyPtrToTimeTime(),
+		matchers.AnyPtrToTimeTime(), pegomock.EqBool(false))
+
+	req = httptest.NewRequest(http.MethodGet, "/key/id1/usage?full=1", nil)
+	testCode(t, req, http.StatusOK)
+	intMock.VerifyWasCalledOnce().Usage(pegomock.AnyString(), matchers.AnyPtrToTimeTime(),
+		matchers.AnyPtrToTimeTime(), pegomock.EqBool(true))
+}
+
 func TestGetKey_ReturnKey(t *testing.T) {
 	initTest(t)
 	pegomock.When(intMock.GetKey(pegomock.AnyString())).
@@ -135,7 +197,7 @@ func TestGetKey_ReturnKey(t *testing.T) {
 	var k api.Key
 	json.Unmarshal(bytes, &k)
 	assert.Equal(t, api.Key{Service: "srv", LastIP: "1.1.1.1"}, k)
-	
+
 	pegomock.When(intMock.GetKey(pegomock.AnyString())).
 		ThenReturn(&api.Key{Key: "aaa", Service: "srv", LastIP: "1.1.1.1"}, nil)
 	req = httptest.NewRequest(http.MethodGet, "/key/id1?returnKey=1", nil)
