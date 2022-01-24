ad9455f981b13f40b7300fa60d7581ce53faae79
Airenas Vaičiūnas <airenass@gmail.com>
2022-01-24 16:24:08 +0200

    Try take auth key from header first

--- internal/pkg/handler/keyExtract.go
+++ internal/pkg/handler/keyExtract.go
@@ -3,7 +3,13 @@ package handler
 import (
 	"net/http"
 	"net/url"
+	"strings"
+
+	"github.com/airenas/go-app/pkg/goapp"
+	"github.com/pkg/errors"
 )
+
+const authHeader = "Authorization"
 
 type keyExtract struct {
 	next http.Handler
@@ -16,19 +22,44 @@ func KeyExtract(next http.Handler) http.Handler {
 	return res
 }
 
+//ServeHTTP tries to extract authorization key from header Authorization: Key <key>
+// if not there then tries to extract from query parameter key=<key>
 func (h *keyExtract) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	keys, ok := r.URL.Query()["key"]
+	key, err := extractKey(r.Header.Get(authHeader))
+	if err != nil {
+		http.Error(w, "Wrong auth header", http.StatusUnauthorized)
+		goapp.Log.Error(errors.Wrap(err, "can't extract key from header"))
+		return
+	}
 	rn, ctx := customContext(r)
-	if ok && len(keys[0]) > 0 {
-		key := keys[0]
+	if key != "" {
+		rn.Header.Del(authHeader)
+	} else {
+		keys, ok := r.URL.Query()["key"]
+		if ok && len(keys[0]) > 0 {
+			key = keys[0]
+			q, _ := url.ParseQuery(rn.URL.RawQuery)
+			q.Del("key")
+			rn.URL.RawQuery = q.Encode()
+		}
+	}
+	if key != "" {
 		ctx.Key = key
 		ctx.Manual = true
-		q, _ := url.ParseQuery(rn.URL.RawQuery)
-		q.Del("key")
-		rn.URL.RawQuery = q.Encode()
 	}
 
 	h.next.ServeHTTP(w, rn)
+}
+
+func extractKey(str string) (string, error) {
+	if str == "" {
+		return "", nil
+	}
+	strs := strings.Split(str, " ")
+	if len(strs) != 2 || strs[0] != "Key" {
+		return "", errors.New("wrong key format, wanted: Key <key>")
+	}
+	return strs[1], nil
 }
 
 func (h *keyExtract) Info(pr string) string {
