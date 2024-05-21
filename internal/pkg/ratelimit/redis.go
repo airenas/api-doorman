68c3bc7241d94de95dd966f949e59332f2f4990e
Airenas Vaiciunas <airenass@gmail.com>
2024-05-21 19:32:41 +0300

    fix: return correct retry after

--- internal/pkg/ratelimit/redis.go
+++ internal/pkg/ratelimit/redis.go
@@ -22,18 +22,20 @@ func NewRedisRateLimiter(url string, secWindow int64) (*RedisRateLimiter, error)
 		return nil, fmt.Errorf("no redis url")
 	}
 	redisdb := redis.NewClient(&redis.Options{
-		Addr: url,
-		MaxRetries: 3,
-		MinIdleConns: 2,
-		IdleTimeout: 5 * time.Minute,
+		Addr:               url,
+		MaxRetries:         3,
+		MinIdleConns:       2,
+		IdleTimeout:        5 * time.Minute,
 		IdleCheckFrequency: time.Minute,
-		PoolSize: 30,
+		PoolSize:           30,
 	})
 	return &RedisRateLimiter{redisdb: redisdb, secWindow: secWindow, url: url}, nil
 }
 
 func (r *RedisRateLimiter) Validate(key string, limit int64, quota int64) (bool, int64, int64, error) {
-	key = fmt.Sprintf("%s:%d", key, time.Now().Unix()/r.secWindow)
+	now := time.Now().Unix()
+	at := now / r.secWindow
+	key = fmt.Sprintf("%s:%d", key, at)
 	val, err := r.redisdb.Get(key).Result()
 	if err != nil && err != redis.Nil {
 		return false, 0, 0, fmt.Errorf("can't get key: %v", err)
@@ -44,7 +46,7 @@ func (r *RedisRateLimiter) Validate(key string, limit int64, quota int64) (bool,
 	}
 	valInt = valInt + quota
 	if valInt >= limit {
-		return false, 0, r.secWindow, nil
+		return false, 0, ((at + 1) * r.secWindow) - now, nil
 	}
 
 	_, err = r.redisdb.TxPipelined(func(pipe redis.Pipeliner) error {
