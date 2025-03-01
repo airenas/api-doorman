42602cf1c6eebf20785327ff28566416c161e228
Airenas Vaiciunas <airenass@gmail.com>
2025-03-01 11:01:30 +0200

    fix: add sample for specific bucket

---
+++ db/migrations/specific/000004_daily_logs.up.sql
@@ -0,0 +1,25 @@
+-- daily_logs materialized view
+
+CREATE MATERIALIZED VIEW daily_logs
+WITH (timescaledb.continuous) AS
+SELECT 
+    time_bucket('1 day', date, 'Europe/Vilnius') AS day,
+    key_id,
+    COUNT(*) AS request_count,
+    COUNT(*) FILTER (WHERE fail) AS failed_requests,
+    SUM(quota_value) FILTER (WHERE fail) AS failed_quota,
+    SUM(quota_value) FILTER (WHERE NOT fail) AS used_quota
+FROM logs
+GROUP BY day, key_id
+WITH NO DATA;
+
+-- add continuous aggregate policy, recalculate every hour
+SELECT add_continuous_aggregate_policy('daily_logs',
+    start_offset => INTERVAL '3 day',
+    end_offset => INTERVAL '1 hour',
+    schedule_interval => INTERVAL '1 hour');
+
+-- turn realtime select feature on
+ALTER MATERIALIZED VIEW daily_logs set (timescaledb.materialized_only = false);
+
+CREATE INDEX idx_daily_logs_key_id ON daily_logs (key_id);
---
+++ db/migrations/specific/000005_monthly_logs.up.sql
@@ -0,0 +1,26 @@
+-- monthly_logs materialized view
+
+-- monthly
+CREATE MATERIALIZED VIEW monthly_logs
+WITH (timescaledb.continuous) AS
+SELECT 
+    time_bucket('1 month', day, 'Europe/Vilnius') AS month,
+    key_id,
+    SUM(request_count) AS request_count,
+    SUM(failed_requests) AS failed_requests,
+    SUM(failed_quota) AS failed_quota,
+    SUM(used_quota) AS used_quota
+FROM daily_logs
+GROUP BY month, key_id
+WITH NO DATA;
+
+-- add continuous aggregate policy, recalculate every day
+SELECT add_continuous_aggregate_policy('monthly_logs',
+    start_offset => INTERVAL '3 month',
+    end_offset => INTERVAL '1 day',
+    schedule_interval => INTERVAL '1 day');    
+
+-- turn realtime select feature on
+ALTER MATERIALIZED VIEW monthly_logs set (timescaledb.materialized_only = false);    
+
+CREATE INDEX idx_monthly_logs_key_id ON monthly_logs (key_id);
